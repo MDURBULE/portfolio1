a182202b36680e0679ca0a92ae6c3362a0a46f7c
MDURBULE <127664742+MDURBULE@users.noreply.github.com>
2024-07-31 18:36:01 +0530

    Update About.jsx
    one correction

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -12,7 +12,7 @@ function About() {
                 I’m Mayur Durbule , dynamic B. Tech student specializing in Electronics and Telecommunication with robust programming foundation . Proficient in C & C++ , Java , Python and SQL , with expertise in basics of  Data Structures and  Algorithms . Skill in technologies including JDBC , Spring boot and React . I’m good with Analytical, Communication , Problem solving,  Critical thinking, Team management , Leadership and I’m also good in Microsoft Office tools, Postman, Workbench , Maven and Chatgpt . I'm eager to apply theoretical knowledge to tackle real-word challenges and keen on continuous learning and growth .  
                 <div className="max-w-screen-2xl container mx-auto  my-5">
                 <h1 className="text-orange-500 font-semibold text-xl">Experience</h1>
-                       <p>I have completed internship which based on core Java in APS Microtech from fed-23 to mar-23</p>
+                       <p>I have completed internship which based on core Java in APS Microtech from feb-23 to mar-23</p>
                         <br />
                         <p>I have completed internship which based on core Java , JDBC , and SQl in Congnifyz from nov-23 to dec-23</p>
                          <br />
